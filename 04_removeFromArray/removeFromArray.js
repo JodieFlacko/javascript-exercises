ab97e33fe98d2b487ffca93e5c79bef59b969759
crlyflacko <peppociccone13@gmail.com>
2024-11-16 18:02:20 +0100

    Fix bug
    Remove multiple values no matter what values are passed as indexes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,10 +3,15 @@ const removeFromArray = function(array, ...args) {
 PSEUDOCODE
 1. Check if there are multiple arguments
 2. If it only is one argument, remove the indexed item
-3. If there are multiple arguments, remove all the indexed values
+3. If there are multiple arguments 
+* Removing them will change the initial index of the items in the original array. Solution: *
+    sort items in args array
+    put them in reverse
+    remove all the indexed values with looping trough the args array
 */
 if(args.length === 1)    array.splice(args[0] - 1, 1);
 else{
+    args.sort().reverse();
     for (const arg of args){
         array.splice(arg - 1, 1);
     }
