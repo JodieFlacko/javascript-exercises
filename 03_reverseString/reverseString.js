2c1c6492a1fd43633249b7f182fb18008424657f
crlyflacko <peppociccone13@gmail.com>
2024-11-16 16:45:16 +0100

    Optimize chaining methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,17 +1,5 @@
 const reverseString = function(word) {
-    /*
-    PSEUDOCODE
-    1. Convert the string into an array
-    2. Use reverse method on the array
-    3. Convert the array into a string
-    4. Return the reversed string
-    */
-
-    let string = word;
-    let array = word.split('');
-    array = array.reverse();
-    string = array.join('');
-    return string;
+    return word.split('').reverse().join('');
 };
 
 // Do not edit below this line
