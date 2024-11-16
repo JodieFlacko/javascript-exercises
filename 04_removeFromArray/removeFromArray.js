71808d487a41d422ae94ebb718fb907dcbea1fec
crlyflacko <peppociccone13@gmail.com>
2024-11-16 17:23:59 +0100

    Use splice method on different line
    Return  on the same line will return original array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, index) {
+/* 
+PSEUDOCODE
+1. Use splice method to remove and indexed element
+*/
+array.splice(index - 1, 1);
+
+return array;
 };
 
 // Do not edit below this line
