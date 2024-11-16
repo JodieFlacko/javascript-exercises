5efbde2bfaaa692386ac9f33ab5c797eb3d19da5
crlyflacko <peppociccone13@gmail.com>
2024-11-16 16:41:55 +0100

    Use string and array methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    /*
+    PSEUDOCODE
+    1. Convert the string into an array
+    2. Use reverse method on the array
+    3. Convert the array into a string
+    4. Return the reversed string
+    */
 
+    let string = word;
+    let array = word.split('');
+    array = array.reverse();
+    string = array.join('');
+    return string;
 };
 
 // Do not edit below this line
