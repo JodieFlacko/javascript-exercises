c57a2d99492ee1142be15badd201b850f32cd84e
crlyflacko <peppociccone13@gmail.com>
2024-11-16 16:19:03 +0100

    Use loop as requested

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0){
+        return "ERROR";
+    }
+    if (num === 0){
+        return "";
+    }
+    let str = string;
+    for (let i = 0; i < num - 1; i++){
+        str += string;
+    }
+    return str;
 };
 
 // Do not edit below this line
