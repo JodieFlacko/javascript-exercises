c92ffcf92596c326d34b97c454e994d2a426bd9d
crlyflacko <peppociccone13@gmail.com>
2024-11-19 12:24:34 +0100

    Convert correctly. All tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // Conversion Formula
+  temp = (temp - 32) * (5/9);
+  // Round a number to 1 decimal place 
+  if(!Number.isInteger(temp)) temp = Math.round(temp * 10) / 10;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp * (9/5) + 32);
+  // Round a number to 1 decimal place 
+  if(!Number.isInteger(temp)) temp = Math.round(temp * 10) / 10;
+
+  return temp;
 };
 
 // Do not edit below this line
