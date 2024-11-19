06ad7374b3f1c72ec185aa5692dc98463a935c67
crlyflacko <peppociccone13@gmail.com>
2024-11-19 11:24:45 +0100

    Feat: sum numbers within the range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(rangeValue1, rangeValue2) {
+    let rangeStart, rangeEnd;
+    if(rangeValue1 < rangeValue2)   {
+        rangeStart = rangeValue1, rangeEnd = rangeValue2;
+    }
+    else{
+        rangeStart = rangeValue2, rangeEnd = rangeValue1;
+    }
+    let sum = rangeStart, counter = rangeStart;
+    while (counter < rangeEnd){
+        sum += counter + 1;
+        counter++;
+    }
+    return sum;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
