36c578cd882c94e6dc24d1e539516be44d58fbdb
crlyflacko <peppociccone13@gmail.com>
2024-11-25 17:12:17 +0100

    Complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, item) =>{
+    return total + item;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, item) =>{
+    return total * item;
+  })
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let result = 1;  
+  for(let i = 0; i < power; i++){
+      result *= num;
+    }
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n === 0) return 1;
+  for(let x = n - 1; x > 0; x--){
+    n *= x;
+  }
+  return n;
 };
 
 // Do not edit below this line
