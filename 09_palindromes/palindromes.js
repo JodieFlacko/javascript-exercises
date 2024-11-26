8a4e7b4a36bb323d81c7e12c1cb96f7a4427e430
crlyflacko <peppociccone13@gmail.com>
2024-11-26 12:04:14 +0100

    Complete. Use RegExp

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+function removePunctuation(string){
+    const array = string.split('');
+    return array.filter(char => {
+        return /^[A-Za-z0-9]+$/.test(char)});
+}
 
+const palindromes = function (str) {
+    string_1 = removePunctuation(str).reverse().join('').toLowerCase();
+    string_2 = removePunctuation(str).join('').toLowerCase();
+
+    // /^[A-Za-z0-9]+$/.test(s) return true if s contains only the specified characters
+    return string_1 === string_2;
 };
 
 // Do not edit below this line
