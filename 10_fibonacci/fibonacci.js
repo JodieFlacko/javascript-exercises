efa6494c2d7e29bf5f6feb9775ef506d525db682
crlyflacko <peppociccone13@gmail.com>
2024-11-26 12:57:16 +0100

    Use recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    // Using recursion
+    
+    if(x < 0)   return 'OOPS';
+    if(x == 0) return +x;
+    let z = x - 1;
+    if(z == 0)  return +x;
+    return fibonacci(x - 1) + fibonacci(x - 2);
 };
 
 // Do not edit below this line
